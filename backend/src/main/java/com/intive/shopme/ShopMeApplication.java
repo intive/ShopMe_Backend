6927a739f7adb6a36480a3819be2f6ae98d46dfe
tolek102 <tolek102@o2.pl>
2018-03-03 21:35:53 +0100

    project initialization

---
+++ backend/src/main/java/com/intive/shopme/ShopMeApplication.java
@@ -0,0 +1,12 @@
+package com.intive.shopme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShopMeApplication.class, args);
+  }
+}
