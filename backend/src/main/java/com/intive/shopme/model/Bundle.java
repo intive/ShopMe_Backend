66d13848b8aeea4149c83b3658a80f3e9058174e
stoicfey <kubawalat@gmail.com>
2018-03-06 22:47:33 +0100

    Moved model package to com.intive.shopme from com.intive.shopme.configuration

--- backend/src/main/java/com/intive/shopme/model/Bundle.java
+++ backend/src/main/java/com/intive/shopme/model/Bundle.java
@@ -1,39 +1,37 @@
 package com.intive.shopme.model;
 
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.util.UUID;
-
-@Entity
-@ApiModel(value = "Offer's Bundle", description = "Represents different bundle types of offer")
-@Builder
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+/**
+ * @author Qba Walat
+ * created on 06/03/2018
+ */
 public class Bundle {
-
-    @ApiModelProperty(value = "Represents bundle's id number")
-    @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
-    private UUID id;
 
     @ApiModelProperty(value = "Represents bundle's description")
     private String description;
-
     @ApiModelProperty(value = "Represents bundle's price")
     private String price;
+
+    public Bundle(String description, String price) {
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 
 }
--- backend/src/main/java/com/intive/shopme/model/User.java
+++ backend/src/main/java/com/intive/shopme/model/User.java
@@ -1,45 +1,58 @@
 package com.intive.shopme.model;
 
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.util.UUID;
-
-@Entity
-@ApiModel(value = "Offer's user", description = "Represents offer's owner.")
-@Builder
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+/**
+ * @author Qba Walat
+ * created on 06/03/2018
+ */
 public class User {
-
-    @ApiModelProperty(value = "Represents user's id number")
-    @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
-    private UUID id;
 
     @ApiModelProperty(value = "Represents user's name", required = true)
     private String name;
-
     @ApiModelProperty(value = "Represents user's email", required = true)
     private String email;
-
     @ApiModelProperty(value = "Represents user's phone number", required = true)
     private String phoneNumber;
-
     @ApiModelProperty(value = "Represents additional information typed by user")
     private String additionalInfo;
 
+    public User(String name, String email, String phoneNumber, String additionalInfo) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.additionalInfo = additionalInfo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+    }
 }
