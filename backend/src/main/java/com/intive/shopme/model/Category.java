c8cbac543e884dccae3335f9085ee2a9d61be7bf
Kuba Walat <kubawalat@gmail.com>
2018-03-09 19:35:48 +0100

    Removed pointless @Getter, @Setter adnotations in model

--- backend/src/main/java/com/intive/shopme/model/Category.java
+++ backend/src/main/java/com/intive/shopme/model/Category.java
@@ -4,8 +4,6 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,16 +14,12 @@ import javax.persistence.Id;
 @Builder
 public @Data
 class Category {
-    
+
     @ApiModelProperty(value = "Represents category's id")
     @Id
     @GeneratedValue
-    @Getter
-    @Setter
     private Long id;
 
     @ApiModelProperty(value = "Represents category's name")
-    @Getter
-    @Setter
     private String name;
 }
--- backend/src/main/java/com/intive/shopme/model/Offer.java
+++ backend/src/main/java/com/intive/shopme/model/Offer.java
@@ -4,8 +4,6 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -18,35 +16,23 @@ class Offer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @ApiModelProperty(value = "Represents offer's unique id number", required = true)
-    @Getter
-    @Setter
     private Long id;
 
     @ApiModelProperty(value = "Represents offer's date of submitting", required = true)
-    @Getter
-    @Setter
     private String date;
 
     @ApiModelProperty(value = "Represents offer's title - passed by user", required = true)
-    @Getter
-    @Setter
     private String title;
 
-    /* @ApiModelProperty(value = "Represents offer's category", required = true)
-     @Getter
-     @Setter
-     private Category category;
-    */
+    @ApiModelProperty(value = "Represents offer's category", required = true)
+    private Category category;
+
     @ApiModelProperty(value = "Represents offer's bundle", required = true)
     @ManyToOne
-    @Getter
-    @Setter
     private Bundle bundle;
 
     @ApiModelProperty(value = "Represents the user who submits this offer", required = true)
     @OneToOne
-    @Getter
-    @Setter
     private User user;
 
 }
