76f95928f47b98ad2fc0f500ec8cd866b6fd6d6c
KrzysztofPawlak <krzysztof.pawlak.tech@gmail.com>
2018-03-10 18:54:05 +0100

    multiple validation errors

--- backend/src/main/java/com/intive/shopme/util/validation/error/ExceptionResponse.java
+++ backend/src/main/java/com/intive/shopme/util/validation/error/ExceptionResponse.java
@@ -1,22 +1,20 @@
 package com.intive.shopme.util.validation.error;
 
+import java.util.HashMap;
+
 public class ExceptionResponse {
     private int code;
-    private String description;
-
-    public int getCode() {
-        return code;
-    }
+    private HashMap errors = new HashMap<String, String>();
 
     public void setCode(int code) {
         this.code = code;
     }
 
-    public String getDescription() {
-        return description;
+    public void add(String field, String msg) {
+        errors.put(field, msg);
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public HashMap getErrors() {
+        return errors;
     }
 }
