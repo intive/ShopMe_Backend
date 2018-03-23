627d7357646918fcac5809d01775f8e57b91e9c3
Maciej Łotysz <lotysz@gmail.com>
2018-03-23 12:28:57 +0100

    Production fixes (#19)
    * Refactoring
    
    * Production defaults
    
    * Typo fox
    
    * Added CORS
    
    * Handle rev proxy for swagger-ui

--- backend/src/main/java/com/intive/shopme/configuration/SwaggerConfig.java
+++ backend/src/main/java/com/intive/shopme/configuration/SwaggerConfig.java
@@ -1,35 +1,91 @@
 package com.intive.shopme.configuration;
 
 import com.google.common.base.Predicates;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.ServletContext;
+
+import static com.intive.shopme.configuration.SwaggerApiConstants.apiInfo;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(Predicates.not(PathSelectors.regex("/error")))
-                .build()
-                .apiInfo(apiInfo());
+    private final ServletContext servletContext;
+
+    @Value("${server.external.host}")
+    private String serverHost;
+
+    @Value("${server.external.url.context}")
+    private String serverContext;
+
+    @Autowired
+    public SwaggerConfig(ServletContext servletContext) {
+        this.servletContext = servletContext;
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("ShopMe by intive Patronage `18 team")
-                .description("ShopMe is a Web Application created during intive Patronage `18 Project")
-                .version("1.1")
-                .build();
+    @Bean
+    Docket api() {
+        return handleReverseProxyMapping(
+                new Docket(DocumentationType.SWAGGER_2)
+                        .select()
+                        .apis(RequestHandlerSelectors.any())
+                        .paths(Predicates.not(PathSelectors.regex("/error")))
+                        .build()
+                        .apiInfo(apiInfo()));
+    }
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+
+    private Docket handleReverseProxyMapping(Docket docket) {
+        return docket
+                .host(serverHost)
+                .pathProvider(createPathProvider());
+    }
+
+    private RelativePathProvider createPathProvider() {
+        return new RelativePathProvider(this.servletContext) {
+            @Override
+            public String getApplicationBasePath() {
+                return serverContext;
+            }
+        };
+    }
+}
+
+final class SwaggerApiConstants {
+
+    private static final String TITLE = "ShopMe by intive Patronage `18 team";
+    private static final String DESC = "ShopMe is a Web Application created during Intive Patronage `18 Project";
+    private static final String VERSION = "1.1";
+
+    private SwaggerApiConstants() {
+    }
+
+    static ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title(TITLE).description(DESC).version(VERSION).build();
     }
 }
