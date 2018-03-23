ddc10600ea60bad99d8da50e11c6324d722a2466
Maciej Sowa <sowa.maciej@outlook.com>
2018-03-23 17:57:22 +0100

    wild card removal

--- backend/src/main/java/com/intive/shopme/model/Offer.java
+++ backend/src/main/java/com/intive/shopme/model/Offer.java
@@ -9,7 +9,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.CascadeType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToOne;
+import javax.persistence.FetchType;
+
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
