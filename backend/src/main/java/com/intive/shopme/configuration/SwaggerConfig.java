39fed4f43a7bd9dd9135c135938c4200ca55f3ad
Maciej  Lotysz <maciej.lotysz@intive.com>
2018-03-23 12:18:05 +0100

    Handle rev proxy for swagger-ui

--- backend/src/main/java/com/intive/shopme/configuration/SwaggerConfig.java
+++ backend/src/main/java/com/intive/shopme/configuration/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.intive.shopme.configuration;
 
 import com.google.common.base.Predicates;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,8 +13,11 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.ServletContext;
 
 import static com.intive.shopme.configuration.SwaggerApiConstants.apiInfo;
 
@@ -20,14 +25,28 @@ import static com.intive.shopme.configuration.SwaggerApiConstants.apiInfo;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private final ServletContext servletContext;
+
+    @Value("${server.external.host}")
+    private String serverHost;
+
+    @Value("${server.external.url.context}")
+    private String serverContext;
+
+    @Autowired
+    public SwaggerConfig(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
+
     @Bean
     Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(Predicates.not(PathSelectors.regex("/error")))
-                .build()
-                .apiInfo(apiInfo());
+        return handleReverseProxyMapping(
+                new Docket(DocumentationType.SWAGGER_2)
+                        .select()
+                        .apis(RequestHandlerSelectors.any())
+                        .paths(Predicates.not(PathSelectors.regex("/error")))
+                        .build()
+                        .apiInfo(apiInfo()));
     }
 
     @Bean
@@ -37,6 +56,21 @@ public class SwaggerConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+
+    private Docket handleReverseProxyMapping(Docket docket) {
+        return docket
+                .host(serverHost)
+                .pathProvider(createPathProvider());
+    }
+
+    private RelativePathProvider createPathProvider() {
+        return new RelativePathProvider(this.servletContext) {
+            @Override
+            public String getApplicationBasePath() {
+                return serverContext;
             }
         };
     }
