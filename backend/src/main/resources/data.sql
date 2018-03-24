2fcf9f83fb2748d2a576ca995f7f9e72b228e838
Pawel Bachta <bachta.pawel@gmail.com>
2018-03-24 23:08:29 +0100

    put categories data into CSV file

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,17 +1,1 @@
-INSERT INTO category VALUES ('c60902ffd0344445b7f61e56154d8724', 'budowa');
-INSERT INTO category VALUES ('0b9a432909c549a1821b578be7ec82f3', 'ogród');
-INSERT INTO category VALUES ('6b7a4917aa884807b660028aa32adbdd', 'usługi warsztatowe');
-INSERT INTO category VALUES ('6eb67bbe592f49aab7827e4b294fef00', 'transport');
-INSERT INTO category VALUES ('41bbb195e2e74152886e6a62213ea16b', 'fotografia');
-INSERT INTO category VALUES ('86f58f3e7191403fb53a6fc57035e3ae', 'grafika');
-INSERT INTO category VALUES ('487600e0e52a4fdb9ac7322d0d0830d7', 'marketing i reklama');
-INSERT INTO category VALUES ('ae544ab4ac754e6d9871832535b28a4f', 'programowanie');
-INSERT INTO category VALUES ('16574a5437d149cd921f4a851fbdb901', 'księgowość');
-INSERT INTO category VALUES ('b6d50b1929d34ae29e361b81a1545ff0', 'prawo');
-INSERT INTO category VALUES ('33291e047d544ed1bf32501dfbf4874c', 'tłumaczenia');
-INSERT INTO category VALUES ('91cf684f99f04db9ad4e3d41069e3c92', 'firma i biuro');
-INSERT INTO category VALUES ('0a61aa476b9a44858edfc3816bd12924', 'naprawa i serwis');
-INSERT INTO category VALUES ('e43f8db9c7f84f83be2911bc85bc0c17', 'prace domowe');
-INSERT INTO category VALUES ('048340ebfa484171968d309ed6cfef8a', 'korepetycje');
-INSERT INTO category VALUES ('621c48bae40444e598c8cd7d012ae53b', 'zespoły i muzyka');
-INSERT INTO category VALUES ('5d214c0195c34ec48f6851dfb80b191c', 'inne');
+INSERT INTO category (ID , NAME ) SELECT ID, NAME FROM CSVREAD('classpath:data/categories.csv');
