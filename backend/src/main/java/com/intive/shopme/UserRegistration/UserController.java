9b06b23703c684fca4f42f252a366634c683939d
Pawel Bachta <bachta.pawel@gmail.com>
2018-03-28 20:13:07 +0200

    PRODPFIP-139 user registration form

---
+++ backend/src/main/java/com/intive/shopme/UserRegistration/UserController.java
@@ -0,0 +1,33 @@
+package com.intive.shopme.UserRegistration;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+import static com.intive.shopme.config.ApiUrl.USERS;
+
+@RestController
+@RequestMapping(value = USERS)
+@Api(value = "users", description = "REST API for users operation", tags = "Users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @ApiOperation("Saves new users")
+    public void add(@RequestBody User user) {
+        user.setId(UUID.randomUUID());
+        userService.add(user);
+    }
+}
---
+++ backend/src/main/java/com/intive/shopme/UserRegistration/UserService.java
@@ -0,0 +1,23 @@
+package com.intive.shopme.UserRegistration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void add(User user) {
+        userRepository.save(user);
+    }
+}
