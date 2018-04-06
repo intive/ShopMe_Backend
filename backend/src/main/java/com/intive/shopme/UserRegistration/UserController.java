c09ac14303969e9a7bca8912b2714d1e7f0d7b3d
Andrzej Szul <34945957+zerg-rush@users.noreply.github.com>
2018-04-06 22:08:37 +0200

    check invoice data presence if checkbox invoice request selected

--- backend/src/main/java/com/intive/shopme/UserRegistration/UserController.java
+++ backend/src/main/java/com/intive/shopme/UserRegistration/UserController.java
@@ -3,6 +3,8 @@ package com.intive.shopme.UserRegistration;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,20 +16,32 @@ import static com.intive.shopme.config.ApiUrl.USERS;
 
 @RestController
 @RequestMapping(value = USERS)
-@Api(value = "users", description = "REST API for users operation", tags = "Users")
+@Api(value = "user", description = "REST API for users operations", tags = "Users")
 class UserController {
 
-    private final UserService userService;
+    private final UserService service;
 
     @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public UserController(UserService service) {
+        this.service = service;
     }
 
     @PostMapping
-    @ApiOperation("Saves new users")
-    public void add(@RequestBody Users users) {
-        users.setId(UUID.randomUUID());
-        userService.add(users);
+    @ApiOperation("Saves new user")
+    public Users add(@RequestBody Users user) {
+        user.setId(UUID.randomUUID());
+        return service.add(user);
     }
+
+    @ApiOperation(value = "Returns user by id")
+    @GetMapping(value = "{id}")
+    public Users get(@PathVariable UUID id) {
+        Users result = service.get(id);
+        if (result != null) {
+            return result.hidePassword();
+        } else {
+            return null;
+        }
+    }
+
 }
---
+++ backend/src/main/java/com/intive/shopme/UserRegistration/ValidInvoiceIfInvoiceRequestedValidator.java
@@ -0,0 +1,37 @@
+package com.intive.shopme.UserRegistration;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@Component
+public class ValidInvoiceIfInvoiceRequestedValidator implements
+        ConstraintValidator<ValidInvoiceIfInvoiceRequested, Users> {
+
+    @Override
+    public void initialize(ValidInvoiceIfInvoiceRequested constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(Users value, ConstraintValidatorContext context) {
+        if (value.getInvoiceRequest().booleanValue()) {
+            Invoice invoice = value.getInvoice();
+            boolean result = (invoice != null) &&
+                    !StringUtils.isEmpty(invoice.getCompanyName()) &&
+                    !StringUtils.isEmpty(invoice.getNip());
+
+            Address invoiceAddress = invoice.getInvoiceAddress();
+            result &= (invoiceAddress != null) &&
+                    !StringUtils.isEmpty(invoiceAddress.getStreet()) &&
+                    !StringUtils.isEmpty(invoiceAddress.getNumber()) &&
+                    !StringUtils.isEmpty(invoiceAddress.getCity()) &&
+                    !StringUtils.isEmpty(invoiceAddress.getZipCode());
+            return result;
+        } else {
+            return true;
+        }
+    }
+
+}
