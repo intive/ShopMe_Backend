92cadf0dea0d262d690790ca3986005597219f97
Maciej Sowa <sowa.maciej@outlook.com>
2018-04-11 15:29:17 +0200

    RuntimeException handling

--- backend/src/main/java/com/intive/shopme/controller/CategoryController.java
+++ backend/src/main/java/com/intive/shopme/controller/CategoryController.java
@@ -7,13 +7,16 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 import static com.intive.shopme.config.ApiUrl.CATEGORIES;
 
@@ -45,4 +48,9 @@ public class CategoryController {
         return service.add(category);
     }
 
+    @DeleteMapping(value = "{id}")
+    //TODO delete it after manual testing
+    public void delete(@PathVariable UUID id) {
+        throw new RuntimeException();
+    }
 }
--- backend/src/main/java/com/intive/shopme/util/validation/error/ErrorHandlingController.java
+++ backend/src/main/java/com/intive/shopme/util/validation/error/ErrorHandlingController.java
@@ -1,5 +1,7 @@
 package com.intive.shopme.util.validation.error;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,10 +11,16 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 @ControllerAdvice
 public class ErrorHandlingController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ErrorHandlingController.class);
+    private static final String VALIDATION_DESCRIPTION_JSON_KEY = "message";
+    private static final String ERROR_ID_JSON_KEY = "id";
 
     @ExceptionHandler(value = {ConstraintViolationException.class})
     public ResponseEntity<ValidationExceptionResponse> handleInvalidInput(ConstraintViolationException exception) {
@@ -41,5 +49,18 @@ public class ErrorHandlingController {
     @ResponseBody
     public ErrorResponse handleAlreadyExistException(AlreadyExistException exception) {
         return new ErrorResponse(HttpStatus.CONFLICT, exception.getName());
+    }
+
+    @ExceptionHandler(value = {RuntimeException.class})
+    @ResponseBody
+    public ResponseEntity handleRuntimeException(RuntimeException exception) {
+        UUID id = UUID.randomUUID(); //TODO change UUID -> var
+        LOGGER.error("ID: {}", id, exception);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(createErrorResponseBody(id.toString(), exception.getMessage()));
+    }
+
+    private static Map<String, String> createErrorResponseBody(String id, String message) {
+        return Map.of(ERROR_ID_JSON_KEY, id, VALIDATION_DESCRIPTION_JSON_KEY, message);
     }
 }
