9c9ed6dead86e2c4f54376a31de4615480ca0e1d
KrzysztofPawlak <krzysztof.pawlak.tech@gmail.com>
2018-04-15 10:19:09 +0200

    category db-view

--- backend/src/main/java/com/intive/shopme/category/CategoryController.java
+++ backend/src/main/java/com/intive/shopme/category/CategoryController.java
@@ -1,10 +1,15 @@
 package com.intive.shopme.category;
 
+import com.intive.shopme.category.model.db.Category;
+import com.intive.shopme.category.model.view.CategoryView;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import static com.intive.shopme.configuration.api.ApiUrl.CATEGORIES;
@@ -28,6 +35,9 @@ public class CategoryController {
 
     private final CategoryService service;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public CategoryController(CategoryService service) {
         this.service = service;
     }
@@ -37,8 +47,9 @@ public class CategoryController {
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = SUCCESS)
     })
-    public List<Category> getAllCategories() {
-        return service.getAll();
+    public List<CategoryView> getAllCategories() {
+        List<Category> categories = service.getAll();
+        return convertToView(categories);
     }
 
     @PostMapping
@@ -48,7 +59,27 @@ public class CategoryController {
             @ApiResponse(code = 201, message = CREATED),
             @ApiResponse(code = 409, message = EXISTS)
     })
-    public Category add(@RequestBody Category category) {
-        return service.create(category);
+    public CategoryView add(@RequestBody CategoryView categoryView) {
+        final Category category = convertToModel(categoryView);
+        final Category categoryCreated = service.create(category);
+        return convertToView(categoryCreated);
+    }
+
+    private CategoryView convertToView(final Category category) {
+        CategoryView categoryViews = modelMapper.map(category, CategoryView.class);
+        return categoryViews;
+    }
+
+    private List<CategoryView> convertToView(final Collection<Category> category) {
+        List<CategoryView> categoryViews = new ArrayList<>();
+        category.forEach(
+                object -> categoryViews.add(convertToView(object))
+        );
+        return categoryViews;
+    }
+
+    private Category convertToModel(final CategoryView categoryView) {
+        Category category = modelMapper.map(categoryView, Category.class);
+        return category;
     }
 }
---
+++ backend/src/main/java/com/intive/shopme/category/model/view/CategoryView.java
@@ -0,0 +1,25 @@
+package com.intive.shopme.category.model.view;
+
+import com.intive.shopme.base.model.IdentifiableView;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.validation.constraints.NotEmpty;
+
+@ApiModel(value = "Offer's category", description = "Represents different category types of an offer")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class CategoryView extends IdentifiableView {
+
+    @ApiModelProperty(value = "Represents category's name", required = true, position = 2, example = "inne")
+    @NotEmpty
+    private String name;
+
+    @ApiModelProperty(value = "Represents key for translate operation", required = true, position = 3, example = "others")
+    @NotEmpty
+    private String translateKey;
+}
