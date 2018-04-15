22a5a2e31f523cfa68b94dda720cbd8099c2c312
KrzysztofPawlak <krzysztof.pawlak.tech@gmail.com>
2018-04-15 10:46:51 +0200

    missed files

---
+++ backend/src/main/java/com/intive/shopme/base/model/Identifiable.java
@@ -0,0 +1,22 @@
+package com.intive.shopme.base.model;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.UUID;
+
+@Data
+@MappedSuperclass
+public abstract class Identifiable {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+}
