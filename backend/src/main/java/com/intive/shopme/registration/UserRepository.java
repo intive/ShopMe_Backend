73a19cd1ecfac8a9709c1bceb83316166e9387cb
Andrzej Szul <34945957+zerg-rush@users.noreply.github.com>
2018-04-23 12:26:09 +0200

    UserRepository changed from public to package private

--- backend/src/main/java/com/intive/shopme/registration/UserRepository.java
+++ backend/src/main/java/com/intive/shopme/registration/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<DbUser, UUID> {
+interface UserRepository extends JpaRepository<DbUser, UUID> {
 
     List<DbUser> findUserByEmail(String email);
 }
