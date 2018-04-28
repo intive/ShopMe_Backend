6664db856bd3c839aed63c138aff3fd68ce16a88
KrzysztofPawlak <krzysztof.pawlak.tech@gmail.com>
2018-04-28 13:36:05 +0200

    remove callSuper from toString

--- backend/src/main/java/com/intive/shopme/model/db/DbVoivodeship.java
+++ backend/src/main/java/com/intive/shopme/model/db/DbVoivodeship.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotEmpty;
 @Table(name = "VOIVODESHIP")
 @Data
 @EqualsAndHashCode
-@ToString(callSuper = true)
+@ToString
 public class DbVoivodeship {
 
     @Id
