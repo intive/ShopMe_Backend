0f26f2d3eaf9e60a09a64db8c66d263fcd980d4a
KrzysztofPawlak <krzysztof.pawlak.tech@gmail.com>
2018-05-03 21:18:26 +0200

    fix constraint name

--- backend/src/main/resources/db/migration/V0.5__voivodeship_category_remove_uuid.sql
+++ backend/src/main/resources/db/migration/V0.5__voivodeship_category_remove_uuid.sql
@@ -10,12 +10,12 @@ alter table VOIVODESHIP add primary key (NAME);
 
 alter table OFFER rename column CATEGORY_ID to CATEGORY_NAME;
 alter table OFFER alter column CATEGORY_NAME type VARCHAR(30);
-alter table OFFER add constraint FK_OFFER_CATEGORY FOREIGN KEY(CATEGORY_NAME) references CATEGORY;
+alter table OFFER add constraint FK_OFFER_CATEGORY FOREIGN KEY (CATEGORY_NAME) references CATEGORY;
 
 alter table OWNER rename column VOIVODESHIP_ID to VOIVODESHIP_NAME;
 alter table OWNER alter column VOIVODESHIP_NAME type VARCHAR(30);
-alter table OWNER add constraint FK_USERS_VOIVODESHIP FOREIGN KEY (VOIVODESHIP_NAME) references VOIVODESHIP;
+alter table OWNER add constraint FK_OWNER_VOIVODESHIP FOREIGN KEY (VOIVODESHIP_NAME) references VOIVODESHIP;
 
 alter table USERS rename column VOIVODESHIP_ID to VOIVODESHIP_NAME;
 alter table USERS alter column VOIVODESHIP_NAME type VARCHAR(30);
-alter table USERS add constraint FK_OWNER_VOIVODESHIP FOREIGN KEY (VOIVODESHIP_NAME) references VOIVODESHIP;
+alter table USERS add constraint FK_USERS_VOIVODESHIP FOREIGN KEY (VOIVODESHIP_NAME) references VOIVODESHIP;
