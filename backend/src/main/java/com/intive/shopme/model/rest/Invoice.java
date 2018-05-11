2743cedf57818a0fd8171ff03344ad4621e2791d
Pawel Bachta <bachta.pawel@gmail.com>
2018-05-11 10:18:34 +0200

    PRODPFIP-323 registration form validation

--- backend/src/main/java/com/intive/shopme/model/rest/Invoice.java
+++ backend/src/main/java/com/intive/shopme/model/rest/Invoice.java
@@ -1,12 +1,19 @@
 package com.intive.shopme.model.rest;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.intive.shopme.validation.SpecialCharacterCheck;
+import com.intive.shopme.validation.WhiteSpaceTabulatorCheck;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.util.UUID;
+
+import static com.intive.shopme.config.AppConfig.NIP_CODE_FORMAT;
 
 @Data
 @ApiModel(value = "Invoice data", description = "Represents data for invoice")
@@ -15,12 +22,18 @@ public class Invoice {
     @ApiModelProperty(value = "Represents unique id number", position = 1, example = "5d214c01-95c3-4ec4-8f68-51dfb80b191c")
     private UUID id;
 
+    @NotEmpty
+    @SpecialCharacterCheck
+    @WhiteSpaceTabulatorCheck
     @ApiModelProperty(value = "Represents user's company name", required = true, position = 2, example = "Januszex Sp.z.o.")
     private String companyName;
 
+    @NotEmpty
+    @Pattern(regexp = NIP_CODE_FORMAT, message = "NIP number should contain only digits and be in format: xxx-xxx-xx-xx ")
     @ApiModelProperty(value = "Represents user's company NIP number", required = true, position = 3, example = "123-456-78-90")
     private String nip;
 
+    @Valid
     @ApiModelProperty(value = "Represents user's company address", required = true, position = 4)
     private Address invoiceAddress;
 
--- backend/src/main/java/com/intive/shopme/model/rest/User.java
+++ backend/src/main/java/com/intive/shopme/model/rest/User.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -13,7 +14,15 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.UUID;
 
+import static com.intive.shopme.config.AppConfig.ACCEPTABLE_ONLY_DIGITS;
+import static com.intive.shopme.config.AppConfig.AT_LEAST_ONE_DIGIT_OCCURS;
+import static com.intive.shopme.config.AppConfig.AT_LEAST_ONE_UPPER_CASE_LETTER_OCCURS;
+import static com.intive.shopme.config.AppConfig.BANK_ACCOUNT_LENGTH;
 import static com.intive.shopme.config.AppConfig.DIGITS_NOT_ACCEPTABLE;
+import static com.intive.shopme.config.AppConfig.PASSWORD_MAX_LENGTH;
+import static com.intive.shopme.config.AppConfig.PASSWORD_MIN_LENGTH;
+import static com.intive.shopme.config.AppConfig.PHONE_NUMBER_MAX_LENGTH;
+import static com.intive.shopme.config.AppConfig.PHONE_NUMBER_MIN_LENGTH;
 import static com.intive.shopme.config.AppConfig.USER_NAME_MAX_LENGTH;
 import static com.intive.shopme.config.AppConfig.USER_NAME_MIN_LENGTH;
 import static com.intive.shopme.config.AppConfig.USER_SURNAME_MAX_LENGTH;
@@ -31,7 +40,7 @@ public class User {
     @Size(max = USER_NAME_MAX_LENGTH, message = "User's name contains too much characters, maximum is " + USER_NAME_MAX_LENGTH + ".")
     @SpecialCharacterCheck
     @WhiteSpaceTabulatorCheck
-    @Pattern(regexp = DIGITS_NOT_ACCEPTABLE)
+    @Pattern(regexp = DIGITS_NOT_ACCEPTABLE, message = "User's name cannot contain any digits")
     @ApiModelProperty(value = "Represents user's name", required = true, position = 2, example = "Jan")
     private String name;
 
@@ -40,24 +49,37 @@ public class User {
     @Size(max = USER_SURNAME_MAX_LENGTH, message = "User's surname contains too much characters, maximum is " + USER_SURNAME_MAX_LENGTH + ".")
     @SpecialCharacterCheck
     @WhiteSpaceTabulatorCheck
-    @Pattern(regexp = DIGITS_NOT_ACCEPTABLE)
+    @Pattern(regexp = DIGITS_NOT_ACCEPTABLE, message = "User's surname cannot contain any digits")
     @ApiModelProperty(value = "Represents user's surname", required = true, position = 3, example = "Kowalski")
     private String surname;
 
+    @NotEmpty
     @Email(message = "User's email has wrong format")
     @ApiModelProperty(value = "Represents user's email", required = true, position = 4, example = "unknown@gmail.com")
     private String email;
 
     @NotEmpty
-    @ApiModelProperty(value = "Represents user's password", required = true, position = 5, example = "password")
+    @Size(min = PASSWORD_MIN_LENGTH, message = "Password contains not enough characters, minimum is " + PASSWORD_MIN_LENGTH + "." )
+    @Size(max = PASSWORD_MAX_LENGTH, message = "Password contains too much characters, maximum is " + PASSWORD_MAX_LENGTH + "." )
+    @Pattern(regexp = AT_LEAST_ONE_DIGIT_OCCURS, message = "Password need contain at least one digit")
+    @Pattern(regexp = AT_LEAST_ONE_UPPER_CASE_LETTER_OCCURS, message = "Password need contain at least one upper case letter")
+    @ApiModelProperty(value = "Represents user's password", required = true, position = 5, example = "Password9")
     private String password;
 
+    @NotEmpty
+    @Size(min = PHONE_NUMBER_MIN_LENGTH, message = "Phone number contains not enough characters, minimum is " + PHONE_NUMBER_MIN_LENGTH + "." )
+    @Size(max = PHONE_NUMBER_MAX_LENGTH, message = "Phone number contains too much characters, maximum is " + PHONE_NUMBER_MAX_LENGTH + "." )
+    @Pattern(regexp = ACCEPTABLE_ONLY_DIGITS, message = "The user's phone number should contain only digits.")
     @ApiModelProperty(value = "Represents user's phone number", required = true, position = 6, example = "0234567890")
     private String phoneNumber;
 
+    @NotEmpty
+    @Size(min = BANK_ACCOUNT_LENGTH, max = BANK_ACCOUNT_LENGTH, message = "Bank account number should contain " + BANK_ACCOUNT_LENGTH + " digits.")
+    @Pattern(regexp = ACCEPTABLE_ONLY_DIGITS, message = "The bank account number should contain only digits.")
     @ApiModelProperty(value = "Represents user's bank account number", required = true, position = 7, example = "01234567890123456789012345")
     private String bankAccount;
 
+    @Valid
     @ApiModelProperty(value = "Represents user's address", required = true, position = 8)
     private Address address;
 
@@ -68,6 +90,7 @@ public class User {
     @ApiModelProperty(value = "Represents request user`s for invoice", required = true, position = 10, example = "true")
     private Boolean invoiceRequest;
 
+    @Valid
     @ApiModelProperty(value = "Represents invoice data for user`s", position = 11)
     private Invoice invoice;
 }
