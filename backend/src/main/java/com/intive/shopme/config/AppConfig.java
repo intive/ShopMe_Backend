b2d9f7db5490b52f33edf2dfd21314428700f6a8
Pawel Bachta <34972580+tolek102@users.noreply.github.com>
2018-05-14 15:25:08 +0200

    PRODPFIP-323 registration form validation (#73)

--- backend/src/main/java/com/intive/shopme/config/AppConfig.java
+++ backend/src/main/java/com/intive/shopme/config/AppConfig.java
@@ -3,8 +3,13 @@ package com.intive.shopme.config;
 public final class AppConfig {
 
     public static final String ACCEPTABLE_TITLE_SEARCH_CHARS = "a-zA-Z0-9ąĄćĆęĘłŁńŃóÓśŚżŻźŹ ";
-    public static final String DIGITS_NOT_ACCEPTABLE = "[^0-9]+";
     public static final String SPECIAL_CHAR_SET = "[-/@#!*$%^&.'_+={}()]";
+    public static final String ZIP_CODE_FORMAT = "^[0-9]{2}(?:-[0-9]{3})$";
+    public static final String NIP_CODE_FORMAT = "^([0-9]{3})(?:-[0-9]{3})(?:-[0-9]{2})(?:-[0-9]{2})$";
+    public static final String DIGITS_NOT_ACCEPTABLE = "[^0-9]+";
+    public static final String ACCEPTABLE_ONLY_DIGITS = "[0-9]+";
+    public static final String AT_LEAST_ONE_DIGIT_OCCURS = "^(?=.*[0-9]).*$";
+    public static final String AT_LEAST_ONE_UPPER_CASE_LETTER_OCCURS = "^(?=.*[A-Z]).*$";
 
     public static final int USER_NAME_MIN_LENGTH = 3;
     public static final int USER_NAME_MAX_LENGTH = 20;
@@ -22,6 +27,18 @@ public final class AppConfig {
 
     public static final String DEFAULT_SORT_FIELD = "date";
     public static final String DEFAULT_SORT_DIRECTION = "DESC";
+
+    public static final int PASSWORD_MIN_LENGTH = 8;
+    public static final int PASSWORD_MAX_LENGTH = 30;
+
+    public static final int PHONE_NUMBER_MIN_LENGTH = 9;
+    public static final int PHONE_NUMBER_MAX_LENGTH = 10;
+
+    public static final int BANK_ACCOUNT_LENGTH = 26;
+
+    public static final int STREET_MIN_LENGTH = 3;
+    public static final int CITY_MAX_LENGTH = 50;
+    public static final int ZIP_CODE_MAX_LENGTH = 6;
 
     private AppConfig() {
     }
--- backend/src/main/java/com/intive/shopme/model/rest/Address.java
+++ backend/src/main/java/com/intive/shopme/model/rest/Address.java
@@ -1,29 +1,51 @@
 package com.intive.shopme.model.rest;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.intive.shopme.validation.SpecialCharacterCheck;
+import com.intive.shopme.validation.WhiteSpaceTabulatorCheck;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
+import static com.intive.shopme.config.AppConfig.CITY_MAX_LENGTH;
+import static com.intive.shopme.config.AppConfig.STREET_MIN_LENGTH;
+import static com.intive.shopme.config.AppConfig.ZIP_CODE_FORMAT;
+import static com.intive.shopme.config.AppConfig.ZIP_CODE_MAX_LENGTH;
+
 @Data
-@ApiModel(value = "Adress data", description = "Represents data for adress")
+@ApiModel(value = "Address data", description = "Represents data for address")
 public class Address {
 
     @ApiModelProperty(value = "Represents unique id number", position = 1, example = "5d214c01-95c3-4ec4-8f68-51dfb80b191c")
     private UUID id;
 
+    @NotEmpty
+    @Size(min = STREET_MIN_LENGTH, message = "Street contains not enough characters, minimum is " + STREET_MIN_LENGTH + ".")
+    @SpecialCharacterCheck
+    @WhiteSpaceTabulatorCheck
     @ApiModelProperty(value = "Represents street name", required = true, position = 2, example = "Niepodległości")
     private String street;
 
+    @NotEmpty
     @ApiModelProperty(value = "Represents home number", required = true, position = 3, example = "12/1")
     private String number;
 
+    @NotEmpty
+    @Size(max = CITY_MAX_LENGTH, message = "City contains too much characters, maximum is " + CITY_MAX_LENGTH + ".")
+    @SpecialCharacterCheck
+    @WhiteSpaceTabulatorCheck
     @ApiModelProperty(value = "Represents city", required = true, position = 4, example = "Szczecin")
     private String city;
 
+    @NotEmpty
+    @Size(max = ZIP_CODE_MAX_LENGTH, message = "ZIP code contains too much characters, maximum is " + ZIP_CODE_MAX_LENGTH + ".")
+    @Pattern(regexp = ZIP_CODE_FORMAT, message = "Zip code should be in format XX-XXX and contain only digits.")
     @ApiModelProperty(value = "Represents ZIP code", required = true, position = 5, example = "70-125")
     private String zipCode;
 
