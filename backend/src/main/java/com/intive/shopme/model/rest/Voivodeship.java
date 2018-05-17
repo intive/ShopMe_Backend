945f91893d0bfff0be22eb21f4d0ecbcedc1421d
Andrzej Szul <34945957+zerg-rush@users.noreply.github.com>
2018-05-18 01:43:18 +0200

    auto set offer ownership in POST /offers/add controller

--- backend/src/main/java/com/intive/shopme/model/rest/Voivodeship.java
+++ backend/src/main/java/com/intive/shopme/model/rest/Voivodeship.java
@@ -2,12 +2,16 @@ package com.intive.shopme.model.rest;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
 @ApiModel(value = "Address voivodeship", description = "Represents possible voivodeship to choose in address")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Voivodeship {
 
     @NotEmpty
