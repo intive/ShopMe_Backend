3cbd0cdf7da1aa53eaa5f0a77c48eb155444cd66
ASzul <34945957+zerg-rush@users.noreply.github.com>
2018-05-17 16:48:20 +0200

    remove unwanted 404 error code from Swagger specs (#77)
    * remove unwanted 404 error code from Swagger specs
    rename controller method getByEmail to existsByEmail
    
    * rephrase method description and correction grammar typo

--- backend/src/main/java/com/intive/shopme/registration/UserController.java
+++ backend/src/main/java/com/intive/shopme/registration/UserController.java
@@ -96,12 +96,9 @@ class UserController extends ConvertibleController<DbUser, User> {
     }
 
     @GetMapping(value = USERS + "/email={email}")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = SUCCESS),
-            @ApiResponse(code = 404, message = NOT_FOUND)
-    })
-    @ApiOperation(value = "Search if user with specified email exist in database")
-    boolean getByEmail(@PathVariable String email) {
+    @ApiResponse(code = 200, message = SUCCESS)
+    @ApiOperation(value = "Check if user with specified email exists in database")
+    boolean existsByEmail(@PathVariable String email) {
         return service.findIfUserExist(email.toLowerCase());
     }
 
