7b8fce0eb74937272c2150f7e0298e910b81b759
Andrzej Szul <34945957+zerg-rush@users.noreply.github.com>
2018-05-18 12:26:05 +0200

    replaced @AllArgsConstructor and @NoArgsConstructor with @JsonCreator

--- backend/src/main/java/com/intive/shopme/model/rest/Voivodeship.java
+++ backend/src/main/java/com/intive/shopme/model/rest/Voivodeship.java
@@ -1,20 +1,23 @@
 package com.intive.shopme.model.rest;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
 @ApiModel(value = "Address voivodeship", description = "Represents possible voivodeship to choose in address")
-@AllArgsConstructor
-@NoArgsConstructor
 public class Voivodeship {
 
     @NotEmpty
     @ApiModelProperty(value = "Represents unique voivodeship name", required = true, example = "WesternPomeranian")
     private String name;
+
+    @JsonCreator
+    public Voivodeship(@JsonProperty("name") String name) {
+        this.name = name;
+    }
 }
