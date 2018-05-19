5fed721e1a41ca5267beaaeb92b0cdfa373a7925
salkidu <salkidu@gmail.com>
2018-05-19 13:36:33 +0200

    removing unused imports

--- backend/src/main/java/com/intive/shopme/registration/ExpiredTokenRepository.java
+++ backend/src/main/java/com/intive/shopme/registration/ExpiredTokenRepository.java
@@ -1,13 +1,9 @@
 package com.intive.shopme.registration;
 
 import com.intive.shopme.model.db.DbExpiredToken;
-import com.intive.shopme.model.db.DbUser;
-import com.intive.shopme.model.rest.UserContext;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 
 @Repository
--- backend/src/main/java/com/intive/shopme/registration/ExpiredTokenService.java
+++ backend/src/main/java/com/intive/shopme/registration/ExpiredTokenService.java
@@ -1,25 +1,13 @@
 package com.intive.shopme.registration;
 
 import com.intive.shopme.model.db.DbExpiredToken;
-import com.intive.shopme.model.db.DbUser;
-import com.intive.shopme.model.rest.ExpiredToken;
 import com.intive.shopme.model.rest.UserContext;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.impl.TextCodec;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Instant;
-import java.util.Date;
 import java.util.UUID;
 
-import static com.intive.shopme.config.AppConfig.EMAIL_CLAIM_NAME;
-import static com.intive.shopme.config.AppConfig.SCOPES_CLAIM_NAME;
 
 @Service
 @Transactional
