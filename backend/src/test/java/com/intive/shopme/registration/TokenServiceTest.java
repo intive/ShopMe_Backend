f98c01298541ff56ea9efcb4e5355c704a72b96e
Maciej Sowa <sowa.maciej@outlook.com>
2018-05-23 02:33:06 +0200

    small changes

--- backend/src/test/java/com/intive/shopme/registration/TokenServiceTest.java
+++ backend/src/test/java/com/intive/shopme/registration/TokenServiceTest.java
@@ -1,8 +1,6 @@
 package com.intive.shopme.registration;
 
 import com.intive.shopme.model.db.DbUser;
-import com.intive.shopme.model.rest.Role;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,39 +12,30 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TokenServiceTest {
 
-    private static final DbUser user = new DbUser();
     private static final UUID ID = UUID.randomUUID();
-    private static final String EMAIL = "typical@email.com";
-    private static final String PASSWORD_ENCODED = "passwd";
+    private static final String CORRECT_PASSWORD = "foo";
     private final TokenService service = new TokenService(PASSWORD_ENCODER, 2000000, "secret");
 
-    @BeforeAll
-    static void init() {
-        user.setId(ID);
-        user.setEmail(EMAIL);
-        user.addRole(Role.USER);
+    @Test
+    void should_throw_badCredentialsException_when_password_is_invalid() {
+        assertThrows(BadCredentialsException.class, () -> service.exchangePasswordForToken(createUser(), "invalidPassword"));
     }
 
     @Test
-    void should_throw_badCredentialsException() {
-        user.setPassword("goodPassword");
-        assertThrows(BadCredentialsException.class, () -> {
-            service.exchangePasswordForToken(user, "wrongPassword");
-        });
+    void should_throw_badCredentialsException_when_user_is_null() {
+        assertThrows(BadCredentialsException.class, () -> service.exchangePasswordForToken(null, CORRECT_PASSWORD));
     }
 
     @Test
     void should_return_token() {
-        user.setPassword("goodPassword");
-        var result = service.exchangePasswordForToken(user, "goodPassword");
-        assertThat(result).isNotNull().isNotEmpty();
+        assertThat(service.exchangePasswordForToken(createUser(), CORRECT_PASSWORD)).isNotEmpty();
     }
 
     private static final PasswordEncoder PASSWORD_ENCODER = new PasswordEncoder() {
 
         @Override
         public String encode(CharSequence charSequence) {
-            return PASSWORD_ENCODED;
+            return "foo";
         }
 
         @Override
@@ -54,4 +43,12 @@ class TokenServiceTest {
             return s.contentEquals(charSequence);
         }
     };
+
+    private DbUser createUser() {
+        var user = new DbUser();
+        user.setId(ID);
+        user.setPassword(CORRECT_PASSWORD);
+        user.setEmail("foo");
+        return user;
+    }
 }
