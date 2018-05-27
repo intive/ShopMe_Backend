52329e0b09ea492980173bbd6148b85743e983c5
Maciej Sowa <sowa.maciej@outlook.com>
2018-05-27 02:03:56 +0200

    changed access to exception

--- backend/src/main/java/com/intive/shopme/config/security/RevokedTokenUseAttemptException.java
+++ backend/src/main/java/com/intive/shopme/config/security/RevokedTokenUseAttemptException.java
@@ -2,7 +2,7 @@ package com.intive.shopme.config.security;
 
 import org.springframework.security.core.AuthenticationException;
 
-public class RevokedTokenUseAttemptException extends AuthenticationException {
+class RevokedTokenUseAttemptException extends AuthenticationException {
 
     public RevokedTokenUseAttemptException(String message) {
         super(message);
