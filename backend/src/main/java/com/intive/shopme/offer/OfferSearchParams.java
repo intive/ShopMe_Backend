7e98562a8b1df9a495a5c8462bad6c36ddb37308
Andrzej Szul <34945957+zerg-rush@users.noreply.github.com>
2018-05-30 18:58:05 +0200

    simplified

--- backend/src/main/java/com/intive/shopme/offer/OfferSearchParams.java
+++ backend/src/main/java/com/intive/shopme/offer/OfferSearchParams.java
@@ -127,7 +127,7 @@ class OfferSearchParams {
                         .replaceAll("  ", " ")
                         .toLowerCase().split(" ");
                 for (String titleKeyword : titleKeywords) {
-                    if (StringUtils.length(titleKeyword) > 1) {
+                    if (titleKeyword.length() > 1) {
                         titleKeywordsIsInvalid = false;
                         builder.with("title", ":", titleKeyword);
                     }
